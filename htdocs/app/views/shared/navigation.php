468a616e6d88675660ead2ec7166f6f7044cfe3a
laronichill <74273468+laronichill@users.noreply.github.com>
2022-01-31 13:42:46 -0500

    Delete htdocs directory

--- htdocs/app/views/shared/navigation.php
+++
@@ -1,7 +0,0 @@
-		<ul>
-			<li><a href='/Animal/index'>Animal index</a></li>
-			<li><a href='/Animal/create'>Animal create</a></li>
-			<li><a href='/Animal/contactInformation'>Contact our animal shelter</a></li>
-			<li><a href='/Main/index'>Main index</a></li>
-			<li><a href='/Main/other'>other method in Main</a></li>
-		</ul>
